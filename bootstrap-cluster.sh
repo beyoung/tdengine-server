490d3b3fe9bb6fe7a096c041e01fd91ef9a9e0ae
twain young <twainyoung@gmail.com>
2020-08-11 10:16:37 +0800

    [feat] add tdengine cluster docker-compose

---
+++ bootstrap-cluster.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sleep 5
+
+taos -f /create_dnodes.sh
---
+++ create_dnodes.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+create dnode "tdengine-002:6030" ;
+create dnode "tdengine-003:6030" ;
+create dnode "tdengine-004:6030" ;
+
